8b68351141af8d8cc6ed6517115cb5d8efe08fdb
Andrés Torres <elfotografo007@gmail.com>
2022-05-30 10:41:54 -0500

    Agregar pregunta de la edad

--- hola.py
+++ hola.py
@@ -5,3 +5,12 @@ print(a)
 
 b = input('Digite nombre: ')
 print('Hola ' + b)
+
+c = input('Digite edad: ')
+edad = int(c)
+
+if edad >= 18:
+    print('Puede tomar y votar')
+else:
+    print('Lo siento mucho, es menor de edad')
+
